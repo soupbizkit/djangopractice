27d7e2fb0513c24f20cecbec6fa5a3227e007a8f
Walter <walter.santacruz@correounivalle.edu.co>
2020-08-30 16:03:52 -0500

    created products's app and modified the model

--- website/products/models.py
+++ website/products/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    title = models.TextField(default='producto name')
-    description = models.TextField(default='this is the description')
-    price = models.TextField(default='200')
-    summary = models.TextField(default='this is cool!')
+    title = models.CharField(max_length=120) #max_length = required
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=20,decimal_places=2)
+    summary = models.TextField()
+    featured = models.BooleanField()
