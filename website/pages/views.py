b6f8f6aa2e8c7961d4309d171ca8ded5dacbf1a8
Walter <walter.santacruz@correounivalle.edu.co>
2020-08-30 19:06:43 -0500

    agrege mas paths a urls y cree mas views en la app de pages

--- website/pages/views.py
+++ website/pages/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def home_view(*args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>") 
+
+def contact_view(*args, **kwargs):
+    return HttpResponse("<h2> Contact page </h2>")
+
+def about_view(*args, **kwargs):
+    return HttpResponse("<h2> About page </h2>")
+
+def social_view(*args, **kwargs):
+    return HttpResponse("<h2> Social page </h2>")
