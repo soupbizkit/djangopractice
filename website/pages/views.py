769995109e59d219fff7d571c4103310e12a9b7f
Walter <walter.santacruz@correounivalle.edu.co>
2021-02-19 17:33:00 -0500

    for loops and context in templates

--- website/pages/views.py
+++ website/pages/views.py
@@ -3,14 +3,23 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request, "home.html", {})
+    my_context = {
+        "bienvenida" : "whatsuppppp",
+        "number": 2131354
+    }
+    return render(request, "home.html", my_context)
     # return HttpResponse("<h1>Hello World</h1>") 
 
 def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {
+        "my_text" : "this is about us",
+        "my_number": 12345,
+        "my_list" : [13,2456,3234,24,54,642]
+    }
+    return render(request, "about.html", my_context)
 
 def social_view(*args, **kwargs):
     return HttpResponse("<h2> Social page </h2>")
