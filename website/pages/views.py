ae95d3b2847dff06b0bae672c10b5851b5520eda
Walter <walter.santacruz@correounivalle.edu.co>
2021-02-19 17:06:37 -0500

    agregué carpeta templates y creé un template base junto a un include de navbar improvisado.

--- website/pages/views.py
+++ website/pages/views.py
@@ -2,14 +2,15 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>") 
+def home_view(request, *args, **kwargs):
+    return render(request, "home.html", {})
+    # return HttpResponse("<h1>Hello World</h1>") 
 
-def contact_view(*args, **kwargs):
-    return HttpResponse("<h2> Contact page </h2>")
+def contact_view(request, *args, **kwargs):
+    return render(request, "contact.html", {})
 
-def about_view(*args, **kwargs):
-    return HttpResponse("<h2> About page </h2>")
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
 
 def social_view(*args, **kwargs):
     return HttpResponse("<h2> Social page </h2>")
