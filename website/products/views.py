b205aa138eb171ce867456205fa85b604a02777d
Walter <walter.santacruz@correounivalle.edu.co>
2021-02-20 17:49:15 -0500

    agregué views para product y templates nuevos.

--- website/products/views.py
+++ website/products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+def product_detail_view(request):
+    obj = Product.objects.get(id=2)
+    context={
+        'object': obj
+    }
+    """context= {
+        'title': obj.title,
+        'description': obj.description
+    }"""
+    return render(request,"product/detail.html", context)
--- website/website/urls.py
+++ website/website/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view, social_view
+from products.views import product_detail_view
 
 
 urlpatterns = [
@@ -23,5 +24,5 @@ urlpatterns = [
     path('', home_view, name='home'),
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
-    path('social/', social_view, name='social'),
+    path('product/', product_detail_view, name='product'),
 ]
