36e2d90f98c750cb64c54cb989eb93ac8a79b86d
Walter <walter.santacruz@correounivalle.edu.co>
2021-02-20 18:16:13 -0500

    nueva view para productos llamado detail.html

--- website/products/views.py
+++ website/products/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Product
 # Create your views here.
 def product_detail_view(request):
-    obj = Product.objects.get(id=2)
+    obj = Product.objects.get(id=1)
     context={
         'object': obj
     }
